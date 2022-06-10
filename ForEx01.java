68502f20bb3f25897d5c57c9934117e03c460668
Arthur-Dent <joonhwanncho@gmail.com>
2022-06-10 16:07:20 +0700

    Add files via upload

---
+++ ForEx01.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class StandardWeight {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	
+
+
+	}
+
+}
---
+++ ForEx02.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class StandardWeight {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	
+
+
+	}
+
+}
+
+/*
+실행
+1
+2
+4
+7
+8
+11
+13
+14
+16
+17
+:
+:
+:
+100
+*/
